a93df48f1f722c39ac77e3ba1cf1dc1944a5d0eb
vesper <vesperjahid@gmail.com>
2019-10-24 05:06:18 +0600

    basic structure of project

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>House Rent Management System</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/reset.css">
+</head>
+<body>
+    
+</body>
+</html>
